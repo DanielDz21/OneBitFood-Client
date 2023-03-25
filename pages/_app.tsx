4bac1e8b495a340d7fa20cc7beff4fda20a5b702
danieldz21 <daniel.da.silva.tj2011@gmail.com>
2023-03-25 18:26:51 -0300

    Initial cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
+import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <>
+      <Head>
+        <title>OneBitFood V2</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 }

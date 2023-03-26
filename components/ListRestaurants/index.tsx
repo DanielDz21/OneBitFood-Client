922881972442a364fb0c7a5eebe231acd88ecf6f
danieldz21 <daniel.da.silva.tj2011@gmail.com>
2023-03-25 21:20:35 -0300

    Create useGetRestaurant hook

--- components/ListRestaurants/index.tsx
+++ components/ListRestaurants/index.tsx
@@ -1,50 +1,23 @@
-import { Row } from "react-bootstrap"
-import { Restaurant } from "./components/Restaurant"
+import { useGetRestaurants } from '@/pages/api/useGetRestaurants';
+import { Alert, Row, Spinner } from 'react-bootstrap'
+import { Restaurant } from './components/Restaurant'
 
 export function ListRestaurants() {
-  const restaurants = [
-    {
-      id: 1,
-      name: "example 1",
-      description: "Javascript Ipsum, Javascript Ipsum e Javascript Ipsum",
-      delivery_tax: "5",
-      image_url: "/restaurant.jpeg",
-      category_title: "Cozinha japonesa",
-    },
-    {
-      id: 2,
-      name: "example 2",
-      delivery_tax: "10",
-      description: "Javascript Ipsum, Javascript Ipsum e Javascript Ipsum",
-      image_url: "/restaurant.jpeg",
-      category_title: "Cozinha mineira",
-    },
-    {
-      id: 3,
-      name: "example 3",
-      delivery_tax: "15",
-      description: "Javascript Ipsum, Javascript Ipsum e Javascript Ipsum",
-      image_url: "/restaurant.jpeg",
-      category_title: "Cozinha vegana",
-    },
-    {
-      id: 4,
-      name: "example 4",
-      delivery_tax: "10",
-      description:
-        "Javascript Ipsum, Javascript Ipsum e Javascript Ipsum Javascript Ipsum, Javascript Ipsum e Javascript Ipsum",
-      image_url: "/restaurant.jpeg",
-      category_title: "Cozinha vegana",
-    },
-  ]
+  const { restaurants, isError, isLoading } = useGetRestaurants();
 
   return (
-    <div className="mt-5">
-      <h3 className="fw-bold">Restaurantes</h3>
+    <>
+      <div className="mt-5">
+        <h3 className="fw-bold">Restaurantes</h3>
 
-      <Row>
-        {restaurants.map((restaurant, i) => <Restaurant key={i} {...restaurant} /> )}
-      </Row>
-    </div>
+        <Row>
+          {isLoading && <Spinner animation="border" />}
+          {isError && <Alert variant='custom-red'>Erro inesperado ao carregar, tente novamente.</Alert>}
+
+
+          {restaurants?.map((restaurant, i) => <Restaurant key={i} {...restaurant} /> )}
+        </Row>
+      </div>
+    </>
   );
 }
---
+++ pages/api/useGetRestaurants.ts
@@ -0,0 +1,17 @@
+import useSWR from "swr";
+
+export function useGetRestaurants() {
+  const fetcher = (url: string) => fetch(url).then((r) => r.json());
+
+  const { data, error } = useSWR<Restaurant[], string>(
+    `${process.env.API_URL}/api/restaurants`,
+    fetcher,
+    { revalidateOnFocus: false }
+    )
+
+    return {
+      restaurants: data,
+      isError: !!error,
+      isLoading: !error && !data
+    }
+}

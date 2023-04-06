484d8cb7b3c46831443f038ede9bbb1f1eba0a9b
DanielDz21 <daniel.da.silva.tj2011@gmail.com>
2023-04-06 14:19:31 -0300

    single quotes

--- components/Categories/Category/index.tsx
+++ components/Categories/Category/index.tsx
@@ -3,18 +3,18 @@ import Link from 'next/link'
 
 export function Category({ title, image_url }: Category) {
   return (
-    <div className="">
-      <Link href={`/restaurants?category=${title}`} className="no-link-style">
+    <div className=''>
+      <Link href={`/restaurants?category=${title}`} className='no-link-style'>
         <Image
           src={image_url}
           alt={title}
           width={300}
           height={200}
-          className="px-1 clickable_effect responsive-image"
+          className='px-1 clickable_effect responsive-image'
         />
       </Link>
 
-      <p className="fw-bold">{title}</p>
+      <p className='fw-bold'>{title}</p>
     </div>
-  );
+  )
 }

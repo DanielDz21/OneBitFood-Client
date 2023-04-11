f6c12336b6d09d5727b8cc20af2ae274d1d44731
DanielDz21 <daniel.da.silva.tj2011@gmail.com>
2023-04-11 13:47:33 -0300

    Filter restaurants by category

--- pages/api/useGetRestaurants.ts
+++ pages/api/useGetRestaurants.ts
@@ -1,10 +1,19 @@
+import { useRouter } from 'next/router'
 import useSWR from 'swr'
 
 export function useGetRestaurants() {
+  const router = useRouter()
+  const { category } = router.query
+
+  let params = ''
+  if (category) {
+    params = `${params == '' ? '?' : '&'}category=${category}`
+  }
+
   const fetcher = (url: string) => fetch(url).then((r) => r.json())
 
   const { data, error } = useSWR<Restaurant[], string>(
-    `${process.env.API_URL}/api/restaurants`,
+    `${process.env.API_URL}/api/restaurants${params}`,
     fetcher,
     { revalidateOnFocus: false }
     )
